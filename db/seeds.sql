ffe01776610dc163d7d72a9d0a2524c3f984d707
mikeemmalee <mmikova91@gmail.com>
2018-04-23 01:13:54 -0700

    Initial file set-up and sql schema

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Classic Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheeseburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Western Bacon Cheeseburger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Swiss Mushroom Burger");
